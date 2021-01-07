d154ec7e9d32cf6e2c3e419a2c9ee4eb54c42f93
Shami Tomita <shamitomita@gmail.com>
2021-01-07 21:32:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
     reversed = []
-    string.split
-    string.each do |word|
+    array = string.split
+    array.each do |word|
     word.reverse
     reversed.push(word)
   end 
