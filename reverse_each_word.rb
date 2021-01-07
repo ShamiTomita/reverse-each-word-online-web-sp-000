7eaaebf8aedd5bae8d6861b915d7b87ec5dfe07a
Shami Tomita <shamitomita@gmail.com>
2021-01-07 21:37:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(string)
     array = string.split
     array.each do |word|
       word.reverse
-      reverse.unshift(word)
+      reverse.push(word)
     end 
   return reversed.join(" ")
 end 
